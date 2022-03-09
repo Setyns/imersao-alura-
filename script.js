c7f948f9abe77a9429dacc754ae26290c5368b79
Cleiton Santos <90425855+Setyns@users.noreply.github.com>
2022-03-09 11:01:55 -0300

    aula 1
    calculadora de media - aula 1 imersao alura

---
+++ script.js
@@ -0,0 +1,10 @@
+var nome = "cleiton"
+var nota1 = 6
+var nota2 = 7
+var nota3 = 9
+var nota4 = 10
+
+var media = (nota1+nota2+nota3+nota4)/4
+
+console.log("bem vindo dev " + nome)
+console.log("a media das notas foram " + media)
